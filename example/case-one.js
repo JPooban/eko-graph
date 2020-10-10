d1496a415b7f67ac12d72e8025514a76d61ce003
Jugkapong Pooban <j.pooban@gmail.com>
2020-10-10 22:20:29 +0700

    feat: initial commit

---
+++ example/case-one.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const { Graph } = require('../lib/graph');
+
+const g = new Graph();
---
+++ tests/graph.spec.js
@@ -0,0 +1,7 @@
+'use strict';
+
+describe('sample test suite', () => {
+  it('sample test case', () => {
+    expect(1).toBe(1);
+  });
+});
